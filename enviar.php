34bfcdaac9ca72a540e2b477f587f618366a818e
TVPRO20 <66882914+TVPRO20@users.noreply.github.com>
2021-09-05 00:17:36 -0500

    first commit

--- enviar.php
+++ enviar.php
@@ -1,10 +1,9 @@
 <?php
-$destino= "jimmy83alex@gmail.com";
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$mensaje = $_POST["mensaje"];
-$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
-mail($destino,"contacto", $contenido);
-header("location:gracias.html");
-
+    $destino= "jimmy83alex@gmail.com";
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $mensaje = $_POST["mensaje"];
+    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
+    mail($destino,"Contacto", $contenido);
+    header("location:gracias.html");
 ?>
