1de2217b08c738ef5be7e6e831fa23ec53c74018
TVPRO20 <66882914+TVPRO20@users.noreply.github.com>
2021-09-04 23:32:18 -0500

    bi

--- enviar.php
+++ enviar.php
@@ -3,7 +3,7 @@ $destino= "jimmy83alex@gmail.com";
 $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
 $mensaje = $_POST["mensaje"];
-$contenido = "nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
+$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
 mail($destino,"contacto", $contenido);
 header("location:gracias.html");
 
